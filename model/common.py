5e278b929393f8283e026684495ee0ac56b84921
Zhongju <125377413+1844822808@users.noreply.github.com>
2025-02-21 23:01:43 +0800

    Update common.py

--- model/common.py
+++ model/common.py
@@ -1,14 +1,5 @@
 import random
 
-# def arugment(img, gt, hflip=True, vflip=True):
-#     if hflip and random.random() < 0.5:
-#         img = img[:, ::-1, :]
-#         gt = gt[:, ::-1, :]
-#     if vflip and random.random() < 0.5:
-#         img = img[::-1, :, :]
-#         gt = gt[::-1, :, :]
-#
-#     return img, gt
 def arugment(img,gt, hflip=True, rot=True):
     hflip = hflip and random.random() < 0.5
     vflip = rot and random.random() < 0.5
@@ -21,23 +12,6 @@ def arugment(img,gt, hflip=True, rot=True):
         gt = gt[::-1, :, :].copy()
 
     return img, gt
-
-# def get_patch(img, gt, patch_size=16):
-#     th, tw = img.shape[:2]
-#
-#     # 计算补丁的长宽
-#     patch_height = patch_size
-#     patch_width = int(patch_height / 4 * 3 )  # 4:3 的长宽比
-#
-#     # 随机选择补丁的左上角坐标
-#     tx = random.randrange(0, (tw - patch_width))
-#     ty = random.randrange(0, (th - patch_height))
-#
-#     # 切割图像和GT以获取补丁
-#     patch_img = img[ty:ty + patch_height, tx:tx + patch_width, :]
-#     patch_gt = gt[ty:ty + patch_height, tx:tx + patch_width, :]
-#     box =(ty, ty + patch_height, tx, tx + patch_width)
-#     return patch_img, patch_gt,box
 
 def get_patch(img, gt, patch_size=16):
     th, tw = img.shape[:2]
